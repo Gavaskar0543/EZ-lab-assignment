7ea7edba4c5a0fb807520317a60c18f492d21af8
Gavaskar0543 <gavaskaraeg@gmail.com>
2023-12-28 21:39:12 +0530

    user signup working fine

--- clientAuthentication/urls.py
+++ clientAuthentication/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("/client/signup", views.signup, name='clientauthsignup'),
-    path("/client/login", views.signin, name='clientauthsignin'),
+    path("client/signup", views.signup, name='clientauthsignup'),
+    path("client/login", views.signin, name='clientauthsignin'),
     # Add other patterns as needed
 ]
--- clientAuthentication/views.py
+++ clientAuthentication/views.py
@@ -1,39 +1,35 @@
-
-from django.shortcuts import render, redirect
+from contextlib import _RedirectStream
+from rest_framework.decorators import api_view, permission_classes, authentication_classes
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
-from django.http import HttpResponse
 
+
+@api_view(['POST'])
+@authentication_classes([SessionAuthentication, BasicAuthentication])  # Use appropriate authentication classes
+@permission_classes([AllowAny])  # Allow any user, including unauthenticated ones
 def signup(request):
     if request.user.is_authenticated:
-        return redirect('/')  # Redirect to the home page if the user is already authenticated
+        return Response({'detail': 'User is already authenticated.'}, status=400)
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserCreationForm(request.data)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password1')  # Use 'password1' as it contains the password
+            print(username,password)
             user = authenticate(request, username=username, password=password)
             if user:
                 login(request, user)
-                return redirect('/')  # Redirect to the home page after successful signup
-    else:
-        form = UserCreationForm()
+                return Response({'detail': 'Signup successful.'})
+        else:
+            print(form.errors)
+            return Response({'detail': 'Invalid form data.'}, status=400)
 
-    return redirect('client/login')
+    return Response({'detail': 'Method not allowed.'}, status=405)
 
 def signin(request):
-    if request.user.is_authenticated:
-        return redirect('/')
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        print(username,password)
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('/')
-        else:
-            return redirect('/')
-    return redirect('/')
+    return Response({'detail': 'login page.'}, status=202)
